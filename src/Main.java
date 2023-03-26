0e245b55a732d214926a96e0288ae9c95dfe292d
Alyona Rukavishnikova <a.rukavichka14@gmail.com>
2023-03-26 23:31:24 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int tiсket = 3698;
+        int x = 20;
+
+        int bonus = tiсket / x;
+
+        System.out.println(bonus);
     }
 }
